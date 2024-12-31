01e0a74a7da57337261574049a15654a0c9a6393
Abrar-Hosny <abrarhosny19@gmail.com>
2024-12-31 19:02:24 +0200

    add Flowbite dependency and configure Tailwind CSS to include Flowbite components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+  content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}",    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+
+    require('flowbite/plugin')
+
+  ],
+  
+
+
 }
