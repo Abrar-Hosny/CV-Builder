6449e0a18da528ae208b15c43d0877028c0957c3
Abrar-Hosny <abrarhosny19@gmail.com>
2024-12-31 20:44:19 +0200

    add PersonalInfo and ProjectSection components to enhance Paper layout

--- src/components/Paper.jsx
+++ src/components/Paper.jsx
@@ -5,6 +5,9 @@ import { IoMdMail } from "react-icons/io";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { TfiWorld } from "react-icons/tfi";
+import PersonalInfo from "./PersonalInfo";
+import ProjectSection from "./ProjectSection";
+
 
 export default function Paper() {
   return (
@@ -12,9 +15,9 @@ export default function Paper() {
       <div className="flex justify-end ">
         <a
           href="#"
-          class="block  min-w-sm p-6 bg-white border  border-gray-200  shadow  min-h-screen dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+          className="block  min-w-sm p-6 bg-white border  border-gray-200  shadow  min-h-screen dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
         >
-          <h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
+          <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
             Abrar Hosny{" "}
           </h1>
           <p className="text-sm text-center ">Alexandria, Egypt</p>
@@ -40,7 +43,11 @@ export default function Paper() {
               <p>Website</p>
             </div>
           </div>
+          <PersonalInfo />
+<ProjectSection/>
+        
         </a>
+
       </div>
     </>
   );
---
+++ src/components/PersonalInfo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function PersonalInfo() {
+  return (
+    <>
+      <div>
+      <h2 className="font-bold">Education</h2>
+      <hr className="border border-black" />
+        <div>
+       
+        <div className="flex justify-between">
+          <h3>Pharos University in Alexandria </h3>
+          <p>Sep. 2021 – May 2025</p>
+        </div>
+        <div className="flex justify-between  text-sm italic ">
+          <h3>Bachelor of Science in Computer Science - GPA: 3.85 </h3>
+          <p>Alexandria, Egypt
+          </p>
+        </div>
+        </div>
+      </div>
+    </>
+  );
+}
