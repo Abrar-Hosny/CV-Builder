237507cdb3038293aa220a91176c5214dc6bd787
Abrar-Hosny <abrarhosny19@gmail.com>
2025-01-01 00:59:34 +0200

    add education and projects sections to Content component with relevant input fields

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -36,6 +36,64 @@ export default function Content() {
   </div>
   </div>
   
+  <div className='w-60 p-2'> 
+     <h1 className='font-bold text-xl mb-2 '>Education</h1>
+  <div class="mb-2">
+    <label for="email" class="block mb-1 text-sm font-medium text-gray-900 ">School</label>
+    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="Pharos University" required />
+  </div>
+  <div class="mb-2">
+    <label for="phone" class="block mb-1 text-sm font-medium text-gray-900 ">Degree</label>
+    <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="Bachelor Degree" required />
+  </div>
+  <div class="mb-2">
+    <label for="email" class="block mb-1 text-sm font-medium text-gray-900 ">Start Date</label>
+    <input type="date" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="abrarhosny@gmil.com" required />
+  </div>
+  <div class="mb-2">
+    <label for="text" class="block mb-1 text-sm font-medium text-gray-900 ">End Date</label>
+    <input type="date" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" required />
+  </div>
+  
+  <div class="mb-2">
+    <label for="text" class="block mb-1 text-sm font-medium text-gray-900 ">Location</label>
+    <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="Alexandria ,Egypt" required />
+  </div>
+  
+  
+  </div>
+  <div className='w-60 p-2'> 
+     <h1 className='font-bold text-xl mb-2 '>Projects</h1>
+  <div class="mb-2">
+    <label for="email" class="block mb-1 text-sm font-medium text-gray-900 ">Company Name</label>
+    <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="Ex: Route" required />
+  </div>
+  <div class="mb-2">
+    <label for="phone" class="block mb-1 text-sm font-medium text-gray-900 ">Position Title</label>
+    <input type="tel" id="phone" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="Bachelor Degree" required />
+  </div>
+  <div class="mb-2">
+    <label for="email" class="block mb-1 text-sm font-medium text-gray-900 ">Start Date</label>
+    <input type="date" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="abrarhosny@gmil.com" required />
+  </div>
+  <div class="mb-2">
+    <label for="text" class="block mb-1 text-sm font-medium text-gray-900 ">End Date</label>
+    <input type="date" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" required />
+  </div>
+  
+  <div class="mb-2">
+    <label for="text" class="block mb-1 text-sm font-medium text-gray-900 ">Location</label>
+    <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="Alexandria ,Egypt" required />
+  </div>
+  
+  <div class="mb-2">
+    <label for="text" class="block mb-1 text-sm font-medium text-gray-900 ">Description</label>
+    <textarea type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2" placeholder="Alexandria ,Egypt" required ></textarea>
+  </div>
+  
+  
+  </div>
+  
 
    </div>
    
